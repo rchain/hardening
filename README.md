90139d85941b7ad77a665d983fca15ae6dad9405
Adam Szkoda <adaszko@gmail.com>
2019-05-30 13:48:12 +0200

    Abandon shared hosts for Terraform state idea

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Hardening Network
-## Setup
 
-1. Set up a shared SSH host to store Terraform state files
-
-    1. `cd tfstate`
-    2. Generate a key for the service account `terraform` in Google Cloud and save it as `account.json` file
-    3. `terraform init` if you're running terraform for the first time
-    4. `terraform plan`
-    5. `terraform apply`
+1. Terraform relies on a service account named `terraform`.  In case it gets deleted somehow, it requires a minimum set of permissions:
+    * Compute Admin
+    * Security Admin
+    * Service Account Admin
+    * Service Account User
+    * Project IAM Admin
+    * Storage Object Admin
+2. `cd hardnet1`
+3. `terraform init`
+4. `terraform plan`
+5. `terraform apply`
 
 **Warning!** The cloud-init script gets executed only once, when the GCP instance
 gets created.  So if there are any changes made to the cloud-init script that
@@ -16,15 +19,3 @@ google_compute_instance.INSTANCE_NAME` and then `terraform apply`.  `terraform
 apply` alone won't work!
 
 `tail -f /var/log/cloud-init-output.log` can be used to see when cloud-init finished job.
-
-2. Prepare a repository on the `tfstate` machine for use with Terraform:
-    1. Copy the git-crypt key file onto the machine: `gcloud compute scp --project "developer-222401" --zone "europe-north1-a" rchain-sre-git-crypt-key terraform@tfstate:`
-    2. SSH onto the machine: `gcloud compute ssh --project "developer-222401" --zone "europe-north1-a" terraform@tfstate`
-    3. Decrypt the repository: `cd hardening; git-crypt unlock ~/rchain-sre-git-crypt-key`
-
-
-3. Create the harnet1 network
-    1. `cd hardnet1`
-    2. `terraform init`
-    3. `terraform plan`
-    4. `terraform apply`
--- hardnet1/main.tf
+++ hardnet1/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "gcs" {
+    bucket  = "rchain-terraform-state"
+    prefix  = "hardnet1"
+    credentials = "account.json"
+  }
+}
+
 variable "resources_name" { default = "hardnet1" }
 variable "gcp_zone" { default = "europe-north1-a" }
 variable "node_count" { default = 1 }
@@ -5,9 +13,64 @@ variable "node_count" { default = 1 }
 provider "google" {
   project = "developer-222401"
   zone    = "${var.gcp_zone}"
+  credentials = "${file("account.json")}"
 }
 
 provider "google-beta" {
   project = "developer-222401"
   zone    = "${var.gcp_zone}"
+  credentials = "${file("account.json")}"
+}
+
+
+resource "google_service_account" "svc_account_node" {
+  account_id = "${var.resources_name}-node"
+}
+
+resource "google_storage_bucket_iam_member" "member" {
+  bucket = "heapdumps.bucket.rchain-dev.tk"
+  role = "roles/storage.objectCreator"
+  member = "serviceAccount:${google_service_account.svc_account_node.email}"
+}
+
+
+resource "google_compute_instance" "node_host" {
+  count = "${var.node_count}"
+  name = "${var.resources_name}-node${count.index}"
+
+  # Nonstandard amount of RAM
+  machine_type = "custom-4-16384"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1810"
+      size = 160
+      type = "pd-ssd"
+    }
+  }
+
+  tags = [
+    "${var.resources_name}-node",
+    "collectd-out",
+    "elasticsearch-out",
+    "logstash-tcp-out",
+    "logspout-http"
+  ]
+
+  service_account {
+    email = "${google_service_account.svc_account_node.email}"
+    scopes = [ "https://www.googleapis.com/auth/cloud-platform" ]
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+
+  connection {
+    type = "ssh"
+    user = "root"
+    private_key = "${file("~/.ssh/google_compute_engine")}"
+  }
 }

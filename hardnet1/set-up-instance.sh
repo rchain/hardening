5e32184650fbffed41561f0ad6d2b31c97330488
Adam Szkoda <adaszko@gmail.com>
2019-06-03 14:38:22 +0200

    Working setup common to all nodes

---
+++ hardnet1/set-up-instance.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -ue
+
+mkdir -p /var/lib/rnode-static
+chmod 710 /var/lib/rnode-static
+chgrp www-data /var/lib/rnode-static
+
+mkdir -p /var/lib/rnode-diag
+chmod 750 /var/lib/rnode-diag
+chgrp www-data /var/lib/rnode-diag
+
+systemctl restart collectd
+
+
+mkdir -p /var/lib/rnode-static
+chmod 710 /var/lib/rnode-static
+chgrp www-data /var/lib/rnode-static
+
+mkdir -p /var/lib/rnode-diag
+chmod 750 /var/lib/rnode-diag
+chgrp www-data /var/lib/rnode-diag
+
+mkdir -p /mnt/heapdumps
+systemctl daemon-reload
+systemctl enable --now mnt-heapdumps.mount
+
+if [[ -z "$(docker ps -q -f name='^logspout$')" ]]; then
+    docker rm logspout || true
+    docker pull gliderlabs/logspout
+    docker run -d --restart=unless-stopped --name=logspout -p 8181:80 -v /var/run/docker.sock:/var/run/docker.sock gliderlabs/logspout
+fi
+
+systemctl reload sshd
+
+systemctl reload nginx

a414840f0e0d07e609e4e3af21a5661e42b88a0f
Adam Szkoda <adaszko@gmail.com>
2019-06-07 15:03:40 +0200

    Parse hostname from instance metadata

--- orchestrator/orchestrator/main.py
+++ orchestrator/orchestrator/main.py
@@ -2,9 +2,31 @@
 
 import sys
 import time
+import asyncio
+import requests
 
 
-def main():
+class Hostname:
+    def __init__(self, hostname):
+        self.instance_name, self.domain = hostname.split('.', maxsplit=1)
+
+    def get_instance_name(self):
+        return self.instance_name
+
+    def get_domain(self):
+        return self.domain
+
+    def with_instance_name(self, new_instance_name):
+        return Hostname('{}.{}'.format(new_instance_name, self.domain))
+
+
+def get_google_cloud_hostname():
+    resp = requests.get('http://metadata.google.internal/computeMetadata/v1/instance/hostname')
+    resp.raise_for_status()
+    return Hostname(resp.content)
+
+
+async def main():
     print("Going to sleep...", file=sys.stderr)
     while True:
         time.sleep(1)
@@ -12,4 +34,7 @@ def main():
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    # pylint: disable=invalid-name
+    event_loop = asyncio.get_event_loop()
+    exit_code = event_loop.run_until_complete(main())
+    sys.exit(exit_code)

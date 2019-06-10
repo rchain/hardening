3619df2e0348ad5eecf1af7d14645ab3e3a6f48d
Adam Szkoda <adaszko@gmail.com>
2019-06-10 13:36:53 +0200

    Add generated Orchestrator SSH keypair

--- hardnet1/set-up-orchestrator.sh
+++ hardnet1/set-up-orchestrator.sh
@@ -2,6 +2,10 @@
 
 pushd /root/hardening
 cp hardnet1/orchestrator.service /etc/systemd/system/orchestrator.service
+
+mkdir -p /root/.ssh
+cp hardnet1/id_orchestrator /root/.ssh/
+
 pushd orchestrator
 python3 -m pip install pipenv
 python3 -m pipenv sync

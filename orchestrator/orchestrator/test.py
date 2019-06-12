7da062498174c4605b4e7c32e0b3d860355efca2
Adam Szkoda <adaszko@gmail.com>
2019-06-12 14:52:12 +0200

    Basic network setup

--- orchestrator/orchestrator/test.py
+++ orchestrator/orchestrator/test.py
@@ -7,6 +7,6 @@ def test_hostname() -> None:
     assert hostname.get_instance_name() == 'hardnet1-orchestrator'
     assert hostname.get_domain() == 'c.developer-222401.internal'
 
-    new_hostname = hostname.with_instance_name('hardnet1-node0')
+    new_hostname = hostname.with_instance_name('hardnet1', 'node0')
     assert new_hostname.get_instance_name() == 'hardnet1-node0'
     assert new_hostname.get_domain() == 'c.developer-222401.internal'

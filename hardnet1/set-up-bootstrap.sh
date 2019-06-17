896633e593d24e19dcc2047fd471c1d69114282f
Adam Szkoda <adaszko@gmail.com>
2019-06-17 13:51:55 +0200

    Add bonds.txt file on the bootstrap node

--- hardnet1/set-up-bootstrap.sh
+++ hardnet1/set-up-bootstrap.sh
@@ -1,3 +1,3 @@
 pushd /root/hardening
-install --mode=644 hardnet1/node-specific/"$(hostname)"/{wallets.txt,rnode.conf} --target-directory=/var/lib/rnode-static/
+install --mode=644 hardnet1/node-specific/"$(hostname)"/{wallets.txt,rnode.conf,bonds.txt} --target-directory=/var/lib/rnode-static/
 popd
